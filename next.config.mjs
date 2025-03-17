56a7f7b930242946a60541b23cd28301780631b3
chamans <chamanladnun@gmail.com>
2025-03-17 15:35:15 +0530

    assets error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
       },
-      output: 'standalone',
-      assetPrefix: '.',
-      trailingSlash: false,
-      
+      reactStrictMode: true,
+      trailingSlash: true,
+      output: 'standalone',  // Ensures Next.js can work as a self-contained app
+      assetPrefix: isProd ? '/_next/' : '', // Ensure correct paths
+      experimental: {
+        outputFileTracingRoot: __dirname, // Fixes missing assets on deployment
+      },
 };
 
 export default nextConfig;
