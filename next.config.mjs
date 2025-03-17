b82c2376b743f03a9c46960f84d4ebfe88ca4c4f
chamans <chamanladnun@gmail.com>
2025-03-17 15:37:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     eslint: {
